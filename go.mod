eb8a43ac09e06165e86f6959bd73c89ba053603e
Alex Raskin <alexraskin@fastmail.fm>
2026-01-28 20:24:03 -0700

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module linkme
+
+go 1.24.1
+
+require github.com/syumai/workers v0.31.0
--- main.go
+++ main.go
@@ -2,23 +2,68 @@ package main
 
 import (
 	"bytes"
-	"io"
+	_ "embed"
+	"html/template"
 	"net/http"
 
 	"github.com/syumai/workers"
 )
 
+//go:embed static/index.html
+var indexHTML string
+
+//go:embed static/style.css
+var styleCSS string
+
+type Link struct {
+	Title string
+	URL   string
+	Icon  string
+}
+
+type PageData struct {
+	Name   string
+	Bio    string
+	Avatar string
+	CSS    template.CSS
+	Links  []Link
+}
+
+var config = PageData{
+	Name:   "alex raskin",
+	Bio:    "devops engineer • cat dad • us-west-1",
+	Avatar: "https://avatars.githubusercontent.com/u/1234567", // replace with your avatar URL
+	Links: []Link{
+		{Title: "stand with iran", URL: "https://standwithiran.org", Icon: "❤️"},
+		{Title: "cease fire today", URL: "https://ceasefiretoday.com/", Icon: "✊"},
+		{Title: "know your rights", URL: "https://www.aclu.org/know-your-rights", Icon: "⚖️"},
+		{Title: "cosmo the cat", URL: "https://cosmothecat.net", Icon: "🐱"},
+		{Title: "github", URL: "https://github.com/alexraskin", Icon: "🐙"},
+		{Title: "twitter", URL: "https://twitter.com/notalexraskin", Icon: "𝕏"},
+	},
+}
+
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, req *http.Request) {
-		msg := "Hello!"
-		w.Write([]byte(msg))
-	})
-	http.HandleFunc("/echo", func(w http.ResponseWriter, req *http.Request) {
-		b, err := io.ReadAll(req.Body)
-		if err != nil {
-			panic(err)
+	tmpl := template.Must(template.New("index").Parse(indexHTML))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path != "/" {
+			http.NotFound(w, req)
+			return
 		}
-		io.Copy(w, bytes.NewReader(b))
+
+		data := config
+		data.CSS = template.CSS(styleCSS)
+
+		var buf bytes.Buffer
+		if err := tmpl.Execute(&buf, data); err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Write(buf.Bytes())
 	})
-	workers.Serve(nil) // use http.DefaultServeMux
+
+	workers.Serve(nil)
 }
